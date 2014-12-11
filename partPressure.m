915a6ffbe2d9fd390b10929326f747c43a80ec9b
Dominik Marti <dom@inik.ch>
2014-12-11 15:16:29 +0100

    partPressure should now be able to calculate an iso_Th-curve outside the bubble-region

--- partPressure.m
+++ partPressure.m
@@ -1,23 +1,13 @@
 % This routine implements Jaro's model to calculate the pressure of the
-% liquid and the vapour phase. You need to run get_r first and feed
-% r to this function. Feed the bubble-radius in um, I will
-% convert it to SI.
+% liquid and the vapour phase. If there is no bubble possible, the function
+% returns the pressure on the iso-Th-curve
 
 function obj = partPressure(obj)
 
 for T_ctr = find(obj.store_p_l==0)
     
     if isnan(obj.r(T_ctr))
-        % It would make sense if the p_l on the iso_Th is calculated,
-        % instead of giving back NaN, but water95 can't do this;
-        if obj.store_T(T_ctr) <= 273.15
-            obj.store_p_l(T_ctr) = NaN;
-        else
-            obj.store_p_l(T_ctr) = pressure(obj.store_rho_overall_at_T(T_ctr), obj.store_T(T_ctr));
-            num2str(T_ctr)
-        end
-        obj.store_p_v(T_ctr) = NaN;
-        
+        obj.store_p_l(T_ctr) = pressure(obj.store_rho_overall_at_T(T_ctr), obj.store_T(T_ctr));
     else
         [P0, rho_liquid, rho_vapour] = saturationPressure(obj.store_T(T_ctr));
 
