c5acdd04d3594c13e4381905085499b1d4f15c7a
Dominik Marti <dom@inik.ch>
2014-12-18 16:33:10 +0100

    Extended get_Th_inf so that it can also take retrograde Th_obs

--- get_Th_inf.m
+++ get_Th_inf.m
@@ -4,15 +4,17 @@
 %              get_Th_inf(Th_obs, r_obs, Th_obs_is_T_bin, T_obs, mineralNumber, Th_inf, V)
 %
 % Calculates the nominal homogenisation temperature and the volume of a
-% fluid inclusion
-%   Th_obs: Th_obs of the inclusion (in C)
+% fluid inclusion. Only Th_obs and r_obs are mandatory inputs.
+%   Th_obs: Th_obs of the inclusion (in deg C)
+%       NB: This can also be a retrograde Th_obs (but this will be slower)
 %   r_obs: r_obs of the inclusion (in um)
 %   Th_obs_is_T_bin: Whether Th_obs corresponds to T_bin (1 or 0)
-%   T_obs: The temperature r_obs was measured at (in C)
+%   T_obs: The temperature r_obs was measured at (in deg C)
 %   mineralNumber: The mineralNumber of the inclusion
-%   Th_inf: The initial guess for Th_inf (in C)
+%   Th_inf: The initial guess for Th_inf (in deg C)
+%       NB: If you provided a retrograde Th_obs, you will still need to
+%       provide the PROgrade initial guess
 %   V: The initial guess for V (in um^3)
-% All arguments are mandatory
 %
 % The inclusionObject will be a fully valid inclusion object matching your 
 % desired values.
@@ -26,12 +28,12 @@ if nargin < 6
 	if nargin < 5
 		[mineralNumber, T_pressureMinimum] = inclusion.set_fi_mineral();
         if nargin < 4
-    		if nargin < 3; Th_obs_is_T_bin = 0; end
+    		if nargin < 3; Th_obs_is_T_bin = 0; end;
             T_obs = T_pressureMinimum - 273.15;
         end
-	else
+    else
 		[mineralNumber, T_pressureMinimum] = inclusion.set_fi_mineral(mineralNumber);
-	end
+    end
     Th_inf = Th_obs + 2;
     V = ones(size(Th_obs))*1e7;
 elseif length(Th_inf) ~= length(Th_obs) || length(V) ~= length(Th_obs)
@@ -42,6 +44,13 @@ else
 end
 
 T_pressureMinimum = T_pressureMinimum - 273.15;
+
+if Th_obs < T_pressureMinimum
+    Th_obs_is_Th_inf_r = 1;
+    if nargin < 6; Th_inf = 2*T_pressureMinimum - Th_obs + 2; end;
+else
+    Th_obs_is_Th_inf_r = 0;
+end
 
 tolerance = 5e-2;
 
@@ -56,9 +65,17 @@ for Th_obs_ctr = length(Th_obs):-1:1
     radius_out_corrected = inclusionObject(Th_obs_ctr).r;
     
     if Th_obs_is_T_bin
-        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+        if Th_obs_is_Th_inf_r
+            Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin_r;
+        else
+            Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+        end
     else
-        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+        if Th_obs_is_Th_inf_r
+            Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp_r;
+        else
+            Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+        end
     end;
 
     Th_inf_step = -1;
@@ -66,7 +83,7 @@ for Th_obs_ctr = length(Th_obs):-1:1
 
     while iterationCounter < 12
         
-        iterationCounter = iterationCounter + 1;
+        iterationCounter = iterationCounter + 1
 
         Th_obs_calculated_old = Th_obs_calculated;
         radius_out_corrected_old = radius_out_corrected;
@@ -98,11 +115,19 @@ for Th_obs_ctr = length(Th_obs):-1:1
                 radius_out_corrected = inclusionObject(Th_obs_ctr).r;
                 
                 if Th_obs_is_T_bin
-                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                    if Th_obs_is_Th_inf_r
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin_r;
+                    else
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                    end
                 else
-                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+                    if Th_obs_is_Th_inf_r
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp_r;
+                    else
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+                    end
                 end;
-
+                
                 if isnan(Th_obs_calculated)
                     if sign(Th_inf_step) == 1;
                         % we probably crossed the flower boundary
@@ -155,9 +180,17 @@ for Th_obs_ctr = length(Th_obs):-1:1
                 radius_out_corrected = inclusionObject(Th_obs_ctr).r;
                 
                 if Th_obs_is_T_bin
-                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                    if Th_obs_is_Th_inf_r
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin_r;
+                    else
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                    end
                 else
-                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+                    if Th_obs_is_Th_inf_r
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp_r;
+                    else
+                        Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+                    end
                 end;
 
                 if isnan(Th_obs_calculated);
@@ -201,10 +234,18 @@ for Th_obs_ctr = length(Th_obs):-1:1
             radius_out_corrected = inclusionObject(Th_obs_ctr).r;
             
             if Th_obs_is_T_bin
-                Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                if Th_obs_is_Th_inf_r
+                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin_r;
+                else
+                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_bin;
+                end
             else
-                Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
-            end;
+                if Th_obs_is_Th_inf_r
+                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp_r;
+                else
+                    Th_obs_calculated = inclusionObject(Th_obs_ctr).T_sp;
+                end
+            end
 
             % If one of the two is NaN here, we have to redo the whole thing,
             % since the combination of the two new values doesn't work.
@@ -233,7 +274,7 @@ for Th_obs_ctr = length(Th_obs):-1:1
         end;
 
         % The vectorised version of the function looks as follows
-        F_vec = [radius_out_corrected; Th_obs_calculated];
+        F_vec = [radius_out_corrected; Th_obs_calculated]
 
         % The next step should take us to a better guess and will be calculated
         % using Newton's method
