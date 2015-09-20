6bcfba128de2dd86af679d4f60b2c2aa83d2fbf5
Dominik Marti <dom@inik.ch>
2015-09-20 21:49:44 +0200

    Changed get_Th_inf to be parallelizable. This should in theory have given a factor 3 improvement. In practice, it was merely 10% faster.

--- get_Th_inf.m
+++ get_Th_inf.m
@@ -36,7 +36,7 @@ function inclusionObject = get_Th_inf(Th_obs, r_obs, Th_obs_is_T_bin, T_obs, min
             [mineralNumber, T_pressureMinimum] = inclusion.set_fi_mineral(mineralNumber);
         end
         Th_inf = Th_obs + 2;
-        V = ones(size(Th_obs))*1e7;
+        V = ones(size(Th_obs))*1e5;
     elseif length(Th_inf) ~= length(Th_obs) || length(V) ~= length(Th_obs)
         inclusionObject = [];
         return
@@ -72,8 +72,8 @@ function inclusionObject = get_Th_inf(Th_obs, r_obs, Th_obs_is_T_bin, T_obs, min
 
         iterationCounter = 0;
 
-        inclusionObject(Th_obs_ctr) = inclusion(Th_inf(Th_obs_ctr), V(Th_obs_ctr), mineralNumber);
-        [r_obs_calculated, Th_obs_calculated] = get_r_obs_and_Th_obs(inclusionObject(Th_obs_ctr), T_obs(Th_obs_ctr), Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
+        curr_inclusionObject(Th_obs_ctr) = inclusion(Th_inf(Th_obs_ctr), V(Th_obs_ctr), mineralNumber);
+        [Th_obs_calculated_old, r_obs_calculated_old] = get_r_obs_and_Th_obs(curr_inclusionObject(Th_obs_ctr), T_obs(Th_obs_ctr), Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
 
         Th_inf_step = -1;
         V_step = -V(Th_obs_ctr)*1e-2;
@@ -81,9 +81,6 @@ function inclusionObject = get_Th_inf(Th_obs, r_obs, Th_obs_is_T_bin, T_obs, min
         while iterationCounter < 12
 
             iterationCounter = iterationCounter + 1;
-
-            Th_obs_calculated_old = Th_obs_calculated;
-            r_obs_calculated_old = r_obs_calculated;
 
             % Sometimes it happens that we cross pressureMinimum. This is bad, 
             % so prevent it from happening
@@ -97,100 +94,69 @@ function inclusionObject = get_Th_inf(Th_obs, r_obs, Th_obs_is_T_bin, T_obs, min
                 V_step = V_step/2;
             end
 
-            Th_obs_calculated = NaN; % This is for NaN-Catching
-            while isnan(Th_obs_calculated)
+            Th_inf_inputs_to_process = [Th_inf(Th_obs_ctr) - Th_inf_step, Th_inf(Th_obs_ctr), Th_inf(Th_obs_ctr) - Th_inf_step];
+            V_inputs_to_process = [V(Th_obs_ctr), V(Th_obs_ctr) - V_step, V(Th_obs_ctr) - V_step];
+            curr_T_obs = T_obs(Th_obs_ctr);
+            
+            parfor i = 1:3
+                curr_inclusionObject(i) = inclusion(Th_inf_inputs_to_process(i), V_inputs_to_process(i), mineralNumber);
+                [r_obs_calculated(i), Th_obs_calculated(i)] = get_r_obs_and_Th_obs(curr_inclusionObject(i), curr_T_obs, Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
+            end;
 
-                % First, do the step in Th_inf-direction
-                Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) - Th_inf_step;
+            if any(isnan(Th_obs_calculated))
+                % we probably crossed the flower boundary. Reduce the step
+                % and try again
+                Th_inf_step = Th_inf_step/2;
+                V_step = V_step/2;
+                iterationCounter = iterationCounter - 1;
+            else
+                % Calculate the derivatives
+                r_obs_grad_Th_inf = (r_obs_calculated_old - r_obs_calculated(1))/Th_inf_step;
+                Th_obs_grad_Th_inf = (Th_obs_calculated_old - Th_obs_calculated(1))/Th_inf_step;
 
-                % This is for NaN-Catching. If the flower-boundary is crossed,
-                % Th_obs and radius_out_corrected become NaN.
-                Th_obs_calculated = NaN;
-                while isnan(Th_obs_calculated)
-                    inclusionObject(Th_obs_ctr) = inclusion(Th_inf(Th_obs_ctr), V(Th_obs_ctr), mineralNumber);
-                    [r_obs_calculated, Th_obs_calculated] = get_r_obs_and_Th_obs(inclusionObject(Th_obs_ctr), T_obs(Th_obs_ctr), Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
-                    if isnan(Th_obs_calculated)
-                        % we probably crossed the flower boundary
-                        Th_inf_step = Th_inf_step/2;
-                        Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) + Th_inf_step;
-                    end
-                end
-
-                % Calculate the derivative
-                r_obs_grad_Th_inf = (r_obs_calculated_old - r_obs_calculated)/Th_inf_step;
-                Th_obs_grad_Th_inf = (Th_obs_calculated_old - Th_obs_calculated)/Th_inf_step;
-                % go back
-                Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) + Th_inf_step;
-
-                % Now do the step in V-direction
-                V(Th_obs_ctr) = V(Th_obs_ctr) - V_step;
-
-                % This is for NaN-Catching. See above for details
-                Th_obs_calculated = NaN;
-                while isnan(Th_obs_calculated)
-                    inclusionObject(Th_obs_ctr) = inclusion(Th_inf(Th_obs_ctr), V(Th_obs_ctr), mineralNumber);
-                    [r_obs_calculated, Th_obs_calculated] = get_r_obs_and_Th_obs(inclusionObject(Th_obs_ctr), T_obs(Th_obs_ctr), Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
-                    if isnan(Th_obs_calculated);
-                        % we probably crossed the flower boundary
-                        V_step = V_step/2;
-                        V(Th_obs_ctr) = V(Th_obs_ctr) + V_step;
-                    end
-                end
-
-                % Calculate the derivative
-                r_obs_grad_V = (r_obs_calculated_old - r_obs_calculated)/V_step;
-                Th_obs_grad_V = (Th_obs_calculated_old - Th_obs_calculated)/V_step;
+                r_obs_grad_V = (r_obs_calculated_old - r_obs_calculated(2))/V_step;
+                Th_obs_grad_V = (Th_obs_calculated_old - Th_obs_calculated(2))/V_step;
 
                 % What we just did is calculate the Jacobian:
                 Jc = [r_obs_grad_Th_inf r_obs_grad_V; Th_obs_grad_Th_inf Th_obs_grad_V];
 
-                % Now go to the new position (We didn't change the volume back ...)
-                Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) - Th_inf_step;
+                % The vectorised version of the function looks as follows
+                F_vec = [r_obs_calculated(3); Th_obs_calculated(3)]
 
-                inclusionObject(Th_obs_ctr) = inclusion(Th_inf(Th_obs_ctr), V(Th_obs_ctr), mineralNumber);
-                [r_obs_calculated, Th_obs_calculated] = get_r_obs_and_Th_obs(inclusionObject(Th_obs_ctr), T_obs(Th_obs_ctr), Th_obs_is_T_bin, Th_obs_is_Th_inf_r);
-
-                % If one of the two is NaN here, we have to redo the whole thing,
-                % since the combination of the two new values doesn't work.
-                if isnan(Th_obs_calculated)
-                    % we probably crossed the flower boundary, so let's go back
-                    % and try again
-                    V(Th_obs_ctr) = V(Th_obs_ctr) + V_step;
-                    V_step = V_step/2;
-                    Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) + Th_inf_step;
-                    Th_inf_step = Th_inf_step/2;
+                if sum(((F_vec - root_pos).*[10; 1]).^2) < tolerance
+                    disp(['Deviation of Th_obs and r_obs smaller than ', num2str(tolerance)]);
+                    break
+                elseif iterationCounter == 12
+                    error('Too many iterations');
                 end
+
+                % The next step should take us to a better guess and will be calculated
+                % using Newton's method
+                % The backslash here is a left multiplication of the inverse of the
+                % Jacobian
+                next_step_vec = Jc\(F_vec - root_pos);
+
+                if sum((next_step_vec.*[100; 1]).^2) < tolerance^2;
+                    disp(['Step size in Th_inf and V smaller than ', num2str(tolerance/100)]);
+                    break
+                end
+                
+                Th_inf(Th_obs_ctr) = Th_inf(Th_obs_ctr) - Th_inf_step;
+                V(Th_obs_ctr) = V(Th_obs_ctr) - V_step;
+
+                Th_inf_step = next_step_vec(1);
+                V_step = next_step_vec(2);
+                
+                Th_obs_calculated_old = Th_obs_calculated(3);
+                r_obs_calculated_old = r_obs_calculated(3);
             end
-
-            % The vectorised version of the function looks as follows
-            F_vec = [r_obs_calculated; Th_obs_calculated];
-
-            if sum(((F_vec - root_pos).*[10; 1]).^2) < tolerance
-                disp(['Deviation of Th_obs and r_obs smaller than ', num2str(tolerance)]);
-                break
-            elseif iterationCounter == 12
-                error('Too many iterations');
-            end
-
-            % The next step should take us to a better guess and will be calculated
-            % using Newton's method
-            % The backslash here is a left multiplication of the inverse of the
-            % Jacobian
-            next_step_vec = Jc\(F_vec - root_pos);
-
-            Th_inf_step = next_step_vec(1);
-            V_step = next_step_vec(2);
-
-            if sum((next_step_vec.*[100; 1]).^2) < tolerance^2;
-                disp(['Step size in Th_inf and V smaller than ', num2str(tolerance/100)]);
-                break
-            end
-
         end
 
         disp(['Th_obs = ', num2str(Th_obs(Th_obs_ctr)) ,'C, r_obs = ', num2str(r_obs(Th_obs_ctr)), 'um']);
         disp(['Th_inf = ', num2str(Th_inf(Th_obs_ctr)), 'C, V = ', num2str(V(Th_obs_ctr)), 'um^3']);
         disp('');
+        
+        inclusionObject(Th_obs_ctr) = curr_inclusionObject(3);
 
     end
 
